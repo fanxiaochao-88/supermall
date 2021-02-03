328fa814d82c35148d7a1cb031ac56a46af6f43b
fanxiaochao <2319480743@qq.com>
2021-02-03 15:38:38 +0800

    '2月3号提交,首页完成到recommend'

---
+++ src/network/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export function request(config) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://106.54.54.237:8000/api/mn',
+    timeout: 5000
+  })
+
+  // 2.1.请求拦截
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+  })
+
+  // 2.2.响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  // 3.发送真正的网络请求
+  return instance(config)
+}
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="recommend">
+    <div v-for='item in recommends' class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendView',
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 14px;
+    padding: 10px 0 20px;
+    border-bottom: 8px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 80%;
+    /* width: 100%; */
+    margin-bottom: 10px;
+  }
+
+  .recommend-item a {
+    display: block;
+  }
+</style>
